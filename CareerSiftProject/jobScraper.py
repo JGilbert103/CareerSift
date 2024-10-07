bdcec787fe7b21131f90ddefc50e18547795290b
JGilbert103 <jgilbert10345@gmail.com>
2024-10-07 17:16:18 -0400

    Updated sifting to grab each link
    Now job scraping gets each link within the 2 pages searched. Starting setup for outputtting the information to a csv and getting the informmation.

--- CareerSiftProject/jobScraper.py
+++ CareerSiftProject/jobScraper.py
@@ -16,6 +16,9 @@ import time;
 from bs4 import BeautifulSoup;
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 
 
 # Link for indeed which is for Computer Science in only Charlotte
@@ -44,15 +47,71 @@ def getJobListings(link):
     
 # Use beautiful soup to retrieve indeed job listings from link
 def getIndeedListings():
-    for i in range(20):
-        response = requests.get(indeed + str((i*10)))
-        if response.status_code == 200:
-         soup = BeautifulSoup(response.content, 'html.parser')
+    allListingsLink = []
+    for i in range(2):
+        driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+        driver.get(indeed + str(i*10))
+        time.sleep(5)
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+        time.sleep(3)  
+        print("Link #" + str(i))
+        jobListingClass = driver.find_elements(By.CSS_SELECTOR, 'li.css-5lfssm.eu4oa1w0')
+        for listing in jobListingClass:
+            try:
+                # Try to find the job title link
+                jobLinkElement = listing.find_element(By.CSS_SELECTOR, 'a.jcs-JobTitle.css-jspxzf.eu4oa1w0')
+                jobLink = jobLinkElement.get_attribute('href')
+                allListingsLink.append(jobLink)
+                print(f"Found job link: {jobLink}")  # Print the link for debugging
+            except Exception:
+                try:
+                    # Handle listings with a more complex structure
+                    jobLinkElement = listing.find_element(By.CSS_SELECTOR, 'h2.jobTitle a')
+                    jobLink = jobLinkElement.get_attribute('href')
+                    allListingsLink.append(jobLink)
+                    print(f"Found complex job link: {jobLink}")
+                except Exception as e:
+                    print(f"No job link found in this listing. Error: {e}")
+        
+        print(f"Page {i+1} complete")
+            
+    print("Indeed complete")
+    # print("ALL job links: ", allListingsLink)
+    driver.quit()
+    getIndeedListingInfo(allListingsLink)
          
-        else:
-            print("Something went wrong in the indeed function")
-         
-# Use beautiful soup to retrieve glassdoor job listings from link
+
+
+#Retrieve the information from getIndeedListings should be every page retreived from the pages searched. This will get the information from each page
+def getIndeedListingInfo(allListingsLinkIndeed):
+    for listing in allListingsLinkIndeed:
+        indeedInformation = []
+        jobTitle = ""
+        jobDesc = ""
+        jobPay = ""
+        jobSite = "Indeed"
+        applyLink = ""
+        jobType = ""
+        jobCompany = ""
+        #Opens the webdriver
+        driver = webdriver.Chrome()
+        driver.get(listing)
+        time.sleep(3)
+        
+
+
+        indeedInformation.append(jobTitle)
+        indeedInformation.append(jobDesc)
+        indeedInformation.append(jobPay)
+        indeedInformation.append(jobSite)
+        indeedInformation.append(applyLink)
+        indeedInformation.append(jobType)
+        indeedInformation.append(jobCompany)
+        writeToIndeedFile(indeedInformation)
+        
+
+
+        # Use beautiful soup to retrieve glassdoor job listings from link
 def getGlassDoorListings():
     return 1
 
@@ -68,8 +127,8 @@ def findDuplicates():
         
 
 # Outputs all information to a csv file after being filtered
-def writeToFile(allJobListings):
-    with open(outputFileName, 'w', newline='') as csvfile:
+def writeToIndeedFile(indeedListingInfo):
+    with open(outputFileName, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
 
@@ -81,19 +140,7 @@ while True:
     #     print(indeed + str((i*10)))
     #     print(response)
     # time.sleep(5)
+    print("Opening Indeed")
+    getIndeedListings()
+    time.sleep(120) # Let the user actually see something!
 
-    driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
-
-    driver.get(indeed)
-
-    time.sleep(5) # Let the user actually see something!
-
-    search_box = driver.find_element_by_name('q')
-
-    search_box.send_keys('ChromeDriver')
-
-    search_box.submit()
-
-    time.sleep(5) # Let the user actually see something!
-
-    driver.quit()
