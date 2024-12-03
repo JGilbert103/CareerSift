5bc6dc654474ecf3bf0a2533f68851d8a5aab56a
othartzell <ohartzel@uncc.edu>
2024-12-02 19:23:44 -0500

    Continuing to develop forms.py
    Created form for user registration

--- CareerSiftApp/forms.py
+++ CareerSiftApp/forms.py
@@ -8,7 +8,27 @@ from database import db
 ## Implement register form functionality
 class RegisterForm(FlaskForm):
     class Meta:
+        csrf = False
+        # Input for username
+        username = StringField('Username', validators=[Length(1, 25)])
 
+        # Input for password
+        password = PasswordField('Password', [DataRequired(message="Enter a password to create an account"),
+        EqualTo('confirmPassword', message="Passwords entered must match")])
+
+        # Input for confirm password
+        confirmPassword = PasswordField('Confirm Password', validators=[Length(min=10, max=25)])
+
+        # Input for email
+        email = StringField('Email',[Email(message='Email is not valid'), DataRequired()])
+
+        # Form submission
+        submit = SubmitField('Submit')
+
+        # Checking if the email is already registered to an account
+        def validateEmail(self, field):
+            if db.session.query(user).filter_by(email=field.data).count() != 0:
+                raise ValidationError('Email registered to another user')
 
 
 ## Implement login form functionality
